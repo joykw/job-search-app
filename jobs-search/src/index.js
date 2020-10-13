d5c0bd6a7780825efc2cd8bdb4b22b41067dc79c
Joy Wanjiku <joywanjiku@Matts-MacBook-Air.local>
2020-10-13 02:33:44 +0100

    added two input text fields for description and location from uses, added checkbox for full-time jobs only

--- jobs-search/src/index.js
+++ jobs-search/src/index.js
@@ -4,11 +4,11 @@ import { Provider } from 'react-redux';
 import store from './store/store';
 import HomePage from './components/HomePage';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '.css/styles.scss';
+import './css/styles.scss';
 
 ReactDOM.render(
-    <Provider store = {store}>
-        <HomePage />
-    </Provider>,
-    document.getElementById('root')
+  <Provider store={store}>
+    <HomePage />
+  </Provider>,
+  document.getElementById('root')
 );
--- jobs-search/src/store/store.js
+++ jobs-search/src/store/store.js
@@ -3,15 +3,14 @@ import thunk from 'redux-thunk';
 import jobsReducer from '../reducers/jobs';
 import errorsReducer from '../reducers/errors';
 
-const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
-
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-    combineReducers({
-        jobs: jobsReducer,
-        errors: errorsReducer
-    }),
-    composeEnhancers(applyMiddleware(thunk))
+  combineReducers({
+    jobs: jobsReducer,
+    errors: errorsReducer
+  }),
+  composeEnhancers(applyMiddleware(thunk))
 );
 
 console.log(store.getState());
